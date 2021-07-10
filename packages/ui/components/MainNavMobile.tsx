b8df863e7487eb72f1a7da6f055280e0fd401b93
John Michael Kuhn Jr <kuhn.johnmichael@gmail.com>
2021-07-10 13:31:22 -0400

    Merge dashboard-ui into preview branch (#58)
    * Added material ui. Removed nextjs starter template. Enabled server-side rendering for material ui.
    
    * Added Sidebar navigation for both desktop and responsive.
    
    * fixed a UI glitch that created extra spacing on bottom on mobile portrait mode
    
    * Create temp
    
    * switch to yarn, remove temp, change logo title
    
    * made drawer fixed and scrollable with increased height to negate glitchy whitespace on mobile browsing. Safari still has some white space on bounce
    
    * Update README.md
    
    * linted importme.css font files
    
    * added theme color meta tag
    
    Co-authored-by: asteria <asteria@syvita.org>

---
+++ packages/ui/components/MainNavMobile.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import MenuIcon from "@material-ui/icons/Menu";
+import { useState } from "react";
+import { CSSTransition } from "react-transition-group";
+import { useClickAway } from "react-use";
+
+import styles from "./MainNavMobile.module.css";
+import transitionStyles from "./transitions.module.css";
+import Drawer from "./Drawer";
+import { useRef } from "react";
+
+interface MainNavMobileProps {
+  children: React.ReactNode;
+}
+
+function MainNavMobile({ children }: MainNavMobileProps) {
+  const [expanded, setExpanded] = useState(false);
+  const [showHamburger, setShowHamburger] = useState(true);
+
+  const ref = useRef(null);
+  useClickAway(ref, () => {
+    console.log(setExpanded(false));
+  });
+
+  return (
+    <div className={styles.mobileDiv}>
+      {showHamburger && (
+        <main className={styles.main}>
+          {children}
+          <MenuIcon
+            className={styles.hamburger}
+            onClick={() => setExpanded(true)}
+          />
+        </main>
+      )}
+      {expanded && <main className={styles.main}>{children}</main>}
+      <CSSTransition
+        in={expanded}
+        timeout={200}
+        classNames={{
+          enterActive: transitionStyles.myNodeEnterActive,
+          enterDone: transitionStyles.myNodeEnter,
+          exitActive: transitionStyles.myNodeExitActive,
+          exitDone: transitionStyles.myNodeExit,
+        }}
+        unmountOnExit
+        onEnter={() => setShowHamburger(false)}
+        onExited={() => setShowHamburger(true)}
+      >
+        {/**
+         * This approach to animation with CSSTransition is a bit hacky
+         * due to enabling classes in Drawer by passing the 'expanded' prop.
+         * This will set the Drawer with an initial size that the classes
+         * in transitions.module.css will override to trigger the transition.
+         * It's not intuitive at all, but it works. I wasn't able to achieve
+         * correct animation when I tried following docs and examples so
+         * this is the approach that worked for me. Potential future refactoring
+         * candidate.
+         */}
+        <Drawer
+          innerRef={ref}
+          className={styles.mobileDrawer}
+          expanded={expanded}
+          handleCloseClick={() => setExpanded(false)}
+        />
+      </CSSTransition>
+    </div>
+  );
+}
+
+export default MainNavMobile;
--- packages/ui/pages/_app.js
+++ packages/ui/pages/_app.js
@@ -1,7 +1,62 @@
-import "../styles/globals.css";
+import React from "react";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../src/theme";
+import { useRouter } from "next/router";
+
+import "../styles/global.css";
+import { Media, MediaContextProvider } from "../components/media";
+import MainNav from "../components/MainNav";
+import styles from "../components/Drawer.module.css";
+import MainNavMobile from "../components/MainNavMobile";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  // Hardcoded routes that will have our Nav. May use a different approach later.
+  const sideBarRoutes = ["/"];
+
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>daoOS</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <meta name="theme-color" content="#181829" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        {sideBarRoutes.includes(router.pathname) ? (
+          <MediaContextProvider>
+            <Media lessThan="md">
+              <MainNavMobile>
+                <Component {...pageProps} />
+              </MainNavMobile>
+            </Media>
+            <Media greaterThan="sm">
+              <MainNav className={styles.fullHeight}>
+                <Component {...pageProps} />
+              </MainNav>
+            </Media>
+          </MediaContextProvider>
+        ) : (
+          <Component {...pageProps} />
+        )}
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
 
 export default MyApp;
