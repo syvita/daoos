63ea684a9b101d236443e2c5c32854dfa3b31a6b
Suleman Mahmood <sulemanmahmood99@gmail.com>
2021-08-31 14:05:05 +0500

    added logout feature

---
+++ packages/ui/pages/logout.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+
+import { userSession } from "../components/auth";
+import PageHeading from "../components/PageHeading";
+
+// Rough draft of what the logout page could look like
+function Login() {
+  useEffect(() => {
+    // Sign the user out
+    userSession.signUserOut();
+
+    // TODO: Redirect the user to the home/landing page
+    window.location.href = "/login";
+  }, []);
+
+  return (
+    <>
+      {/* TODO: Add styling and proper layouts */}
+      <PageHeading>Logging out...</PageHeading>
+    </>
+  );
+}
+
+export default Login;
